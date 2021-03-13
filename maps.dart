0c2d5fbccd8490f2664c9902539ce2e49101716a
Alex Murithi <gitsaleks@gmail.com>
2021-03-13 15:10:04 +0300

    Maps

---
+++ maps.dart
@@ -0,0 +1,25 @@
+void main() {
+    var cars ={
+       //key//  //value//
+       "Model":"Toyota",
+       "Type":"LandCruiser",
+       "Wheels":4
+    };
+  print(cars);
+  
+  Map<String, String> vehicles ={
+     "Model":"Toyota",
+    "Type":"LandCruiser",
+    "Wheels":4.toString()
+  };
+  
+  print(vehicles);
+  
+var gifts = Map<String, String>();
+gifts['first'] = 'partridge';
+gifts['second'] = 'turtledoves';
+gifts['fifth'] = 'golden rings';
+
+  print(gifts);
+  
+  }
