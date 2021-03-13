3b46c742c6b6edf633b30a2323a6bdb58893a14d
Alex Murithi <gitsaleks@gmail.com>
2021-03-13 14:42:49 +0300

    List

---
+++ lists.dart
@@ -0,0 +1,13 @@
+void main() {
+    
+  var lst = [1, 2, 3];
+  
+  List <int> list =[23,45,30];
+  List <int> growableList =[];
+  growableList.add(9);
+  growableList.add(10);
+
+  print(lst);
+  print(list);
+  print(growableList);
+  }
