14310aa05700182cf774ea28351104e1049fc106
Alex Murithi <gitsaleks@gmail.com>
2021-03-13 14:57:28 +0300

    Sets

---
+++ sets.dart
@@ -0,0 +1,12 @@
+void main() {
+    
+  var carModels ={"Toyota","Subaru","Isuzu","BMW"};
+  print(carModels);
+  
+  var cars =<String>{"Isuzu","Toyota","Nissan"};
+  print(cars);
+  
+  Set<String> vehicles ={"Subaru","Mishtubishi","Jeep"};
+  print(vehicles);
+ 
+  }
