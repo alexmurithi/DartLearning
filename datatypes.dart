5c00865fb507ead3f66b68d23bb80c3aae3f8586
Alex Murithi <gitsaleks@gmail.com>
2021-03-13 11:40:14 +0300

    Data types

---
+++ datatypes.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name ='Alex';
+
+  int number =10;
+
+  double fraction =20.67;
+
+  bool choose =20>5;
+
+  print(name);
+  print(number);
+  print(fraction);
+  print(choose);
+
+}
